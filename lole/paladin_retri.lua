137bd7cbbd4fd97130028aad30fe70d5f7f47819
Johannes Miettinen <miettinen.johannes@gmail.com>
2016-03-04 02:08:25 +0200

    Retri rotation improvements

--- lole/paladin_retri.lua
+++ lole/paladin_retri.lua
@@ -1,12 +1,17 @@
 combat_paladin_retri = function()
 	
-	if cleanse_party("Arcane Shock") then return; end
+	--if cleanse_party("Arcane Shock") then return; end
 	
 	if not validate_target() then return; end
 	
 	melee_close_in()
 	
-	if not has_debuff("target", "Judgement of the Crusader") then
+    local jud_on_cd = true;
+	if GetSpellCooldown("Judgement") == 0 then
+        jud_on_cd = false;
+    end
+
+    if not jud_on_cd and not has_debuff("target", "Judgement of the Crusader") then
 		if not has_buff("player", "Seal of the Crusader") then
 			cast_if_nocd("Seal of the Crusader");
 		end
@@ -21,8 +26,9 @@ combat_paladin_retri = function()
 
 	if cast_if_nocd("Crusader Strike") then return end
 
-	if cast_if_nocd("Judgement") then return end
-
+	if not jud_on_cd then
+        CastSpellByName("Judgement");
+    end
 
 end
 
