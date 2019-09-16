219938a359d9df848b677a87afaa6309de4e7c06
Elias Miettinen <eliaspmiettinen@gmail.com>
2019-09-16 21:21:05 +0300

    add shader.*

---
+++ wowibottihookdll/shader.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#pragma comment (lib, "opengl32.lib")
+
+#include <Windows.h>
+#include <gl/gl.h>
+
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <unordered_map>
+
+#include "aux_window.h"
+
+namespace Shader {
+
+	static GLint checkShaderCompileStatus(GLuint shaderId);
+	static const std::string logfilename("shader.log");
+
+};
+
+class ShaderProgram {
+
+	GLuint programHandle_;
+	bool error;
+
+	std::unordered_map<std::string, GLint> uniform_locations;
+
+public:
+
+	GLint checkLinkStatus();
+	ShaderProgram(const std::string &shader_folder);
+
+	bool valid() const;
+
+	void cache_uniform_location(const std::vector<std::string>& uniform_name);
+	GLint get_uniform_location(const std::string& name) const;
+	GLuint programHandle() const { return programHandle_; }
+};
+
+class Render;
+typedef void (*render_initfunc)(Render*);
+typedef void (*render_drawfunc)(Render*);
+
+class Render {
+public:
+	ShaderProgram* shader;
+	GLuint VAOid, VBOid;
+	render_initfunc _init;
+	render_drawfunc _draw;
+	Render(ShaderProgram *p, render_initfunc initf, render_drawfunc drawf) 
+		: VAOid(0), VBOid(0), shader(p), _init(initf), _draw(drawf) {
+
+		_init(this);
+	}
+	void update_buffer(const void* data, size_t s) {
+		glBindBuffer(GL_ARRAY_BUFFER, VBOid);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, s, data);
+	}
+
+	void draw() {
+		_draw(this);
+	}
+
+};
