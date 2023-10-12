adde728cf058840ad64cc20ce2dc0eddf34369f0
Miski <misamatti@gmail.com>
2023-10-12 15:09:18 +0300

    stop the pieru

--- lole/ranged_hunter.lua
+++ lole/ranged_hunter.lua
@@ -94,8 +94,6 @@ function combat_ranged_hunter()
     -- if need_mana() then return end;
     check_buffs();
     set_pet_state();
-    attack()
-    SendChatMessage("pieru", "YELL");
     if validate_target() then
         set_hunters_mark();
         attack();
